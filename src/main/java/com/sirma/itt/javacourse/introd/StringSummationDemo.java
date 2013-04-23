b3a6ffe14105305d1df04ebe7c6f6486ab171a1c
Ventsislav Marinov <vmarinovrs@gmail.com>
2013-04-23 01:27:07 -0700

    ITTE-477 StringSummationDemo Run class for StringSummation.

---
+++ src/main/java/com/sirma/itt/javacourse/introd/StringSummationDemo.java
@@ -0,0 +1,45 @@
+package com.sirma.itt.javacourse.introd;
+
+/**
+ * Run class for {@link StringSummation}
+ * 
+ * @author Ventsislav Marinov
+ */
+public class StringSummationDemo {
+	public static void main(String args[]) {
+		StringSummation stringSummation = new StringSummation("911", "11111");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum()
+				+ " is: "
+ + stringSummation.getSum());
+		stringSummation = new StringSummation("378", "17659");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+		stringSummation = new StringSummation("572", "199569");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+		stringSummation = new StringSummation("199357", "999");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+		stringSummation = new StringSummation("333", "999");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+
+		stringSummation = new StringSummation("33", "999");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+		stringSummation = new StringSummation("9", "9");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+		stringSummation = new StringSummation("99999", "9999");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum()
+				+ " is: "
+ + stringSummation.getSum());
+		stringSummation = new StringSummation("2", "998");
+		System.out.println("The sum of " + stringSummation.getFirstNum() + " and "
+				+ stringSummation.getSecondNum() + " is: " + stringSummation.getSum());
+
+
+	}
+}
