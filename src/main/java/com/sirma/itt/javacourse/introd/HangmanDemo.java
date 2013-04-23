b2493a0429b801d509f3088bf66f2ea4719aa44d
Ventsislav Marinov <vmarinovrs@gmail.com>
2013-04-23 01:32:07 -0700

    ITTE-479 HangmanDemo Run class for Hangman.

---
+++ src/main/java/com/sirma/itt/javacourse/introd/HangmanDemo.java
@@ -0,0 +1,13 @@
+package com.sirma.itt.javacourse.introd;
+
+/**
+ * Run class for {@link Hangman}
+ * 
+ * @author Ventsislav Marinov
+ */
+public class HangmanDemo {
+	public static void main(String args[]) {
+		Hangman hangman = new Hangman();
+		hangman.startGame();
+	}
+}
