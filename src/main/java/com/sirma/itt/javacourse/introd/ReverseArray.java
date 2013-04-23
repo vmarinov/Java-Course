ba9fb7575748014a5919b425cea76d858662543f
Ventsislav Marinov <vmarinovrs@gmail.com>
2013-04-23 01:23:23 -0700

    ITTE-476 Reverse Array Class used to reverse arrays.

---
+++ src/main/java/com/sirma/itt/javacourse/introd/ReverseArray.java
@@ -0,0 +1,31 @@
+package com.sirma.itt.javacourse.introd;
+
+/**
+ * Class used to reverse arrays.
+ *
+ * @author Ventsislav Marinov
+ */
+public class ReverseArray {
+	/**
+	 * Used to reverse a given array.
+	 * 
+	 * @param array
+	 *            The array to be reversed.
+	 * @return Returns the array in back order.
+	 */
+	public static int[] reverse(int[] array) {
+		int firstElement;
+		int secondElement;
+		int temp = 0;
+		for (firstElement = 0, secondElement = array.length - 1; firstElement <= (secondElement - 1);) {
+			temp = array[firstElement];
+			array[firstElement] = array[secondElement];
+			array[secondElement] = temp;
+			firstElement++;
+			secondElement--;
+		}
+		return array;
+	}
+
+
+}
