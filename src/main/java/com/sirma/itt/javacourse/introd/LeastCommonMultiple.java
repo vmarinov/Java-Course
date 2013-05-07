2a3e6338b9cb25768038f30145b562418d5cbf11
Ventsislav Marinov <vmarinovrs@gmail.com>
2013-05-07 00:58:01 -0700

    Changed method modifiers. Removed unused fields and getters/setters.

--- src/main/java/com/sirma/itt/javacourse/introd/LeastCommonMultiple.java
+++ src/main/java/com/sirma/itt/javacourse/introd/LeastCommonMultiple.java
@@ -2,12 +2,10 @@ package com.sirma.itt.javacourse.introd;
 
 /**
  * Class used to determine the least common multiple(lcm) of two numbers.
- * 
+ *
  * @author Ventsislav Marinov
  */
 public class LeastCommonMultiple {
-	private int a;
-	private int b;
 
 	/**
 	 * Used to determine the least common multiple of two numbers.
@@ -18,31 +16,16 @@ public class LeastCommonMultiple {
 	 *            - Second number.
 	 * @return returns the lcm of the two numbers
 	 */
-	public int findLcm(int a, int b) {
-		this.a = a;
-		this.b = b;
+	public static int findLcm(int a, int b) {
+
 		GreatestCommonDivisor greatestCommonDivisor = new GreatestCommonDivisor();
 		// holds the lcm of the two numbers
 		int result = 0;
 		// holds the greatest common divisor
-		int gcd = greatestCommonDivisor.findGcd(this.a, this.b);
+		int gcd = greatestCommonDivisor.findGcd(a, b);
 		result = (a * b) / gcd;
 		return result;
 	}
 
-	public int getA() {
-		return a;
-	}
 
-	public void setA(int a) {
-		this.a = a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setB(int b) {
-		this.b = b;
-	}
 }
